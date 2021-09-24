fe9b2e465a8f1dc84fcc2b8c45c5b423efb7482d
hb <hongbin@xiao100.com>
2021-09-24 14:30:54 +0800

    initnal: publish codes

---
+++ hb_scaffold/bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const lib = require('hb_scaffold-lib')
+const { sub , max} = lib
+
+console.log('你好 123',sub(1,2),max(4,2)) 
---
+++ hb_scaffold/node_modules/hb_scaffold-lib/lib/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sub(a,b){
+        return a + b
+    },
+    max(a,b){
+        return Math.max(a,b)
+    }
+}
